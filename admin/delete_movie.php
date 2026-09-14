cd19e940c507fde5ada81e917a2aaa827f989fed
raymondsondo0-a11y <raymondsondo0@gmail.com>
2026-09-14 20:17:49 +0300

    Remove uploaded media when deleting movies

--- admin/delete_movie.php
+++ admin/delete_movie.php
@@ -1,4 +1,23 @@
 <?php
 require __DIR__ . '/../private/bootstrap.php'; require_admin();
-$id=(int)($_POST['id']??0); if($id){$s=$pdo->prepare('DELETE FROM movies WHERE id=?');$s->execute([$id]);}
+
+$id=(int)($_POST['id']??0);
+if($id){
+    $s=$pdo->prepare('SELECT poster_url, video_url FROM movies WHERE id=?');
+    $s->execute([$id]);
+    $movie=$s->fetch();
+
+    $s=$pdo->prepare('DELETE FROM movies WHERE id=?');
+    $s->execute([$id]);
+
+    if($movie){
+        foreach(['poster_url'=>'posters','video_url'=>'movies'] as $field=>$folder){
+            $url=(string)($movie[$field]??'');
+            if(!str_starts_with($url,'/uploads/'.$folder.'/')) continue;
+            $file=realpath(__DIR__.'/..'.$url);
+            $base=realpath(__DIR__.'/../uploads/'.$folder);
+            if($file && $base && str_starts_with($file,$base.DIRECTORY_SEPARATOR) && is_file($file)) @unlink($file);
+        }
+    }
+}
 header('Location: movies.php'); exit;
